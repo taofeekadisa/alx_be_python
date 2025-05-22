a3dc5aa85a696eb6d30a6300f3f2a76892b1c891
Taofeek Adisa <106744927+taofeekadisa@users.noreply.github.com>
2025-05-22 20:41:50 +0000

    Introduction to python week 3 tasks completed

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 2
+number2 = 3
+
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_saving = monthly_income  - monthly_expenses
+
+print(f"Your monthly savings are ${monthly_saving}.")
+print(f"Projected savings after one year, with interest, is: ${monthly_saving * 12 + (monthly_saving * 12 * 0.05)}.")
