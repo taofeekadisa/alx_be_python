771d633d9faa309a0fd4369494c893a12e57212a
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-08 17:21:16 +0300

    feat:update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -38,7 +38,7 @@ Future date: 2024-04-04
 from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.now().replace(microsecond=0)
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {current_date}")
 
 display_current_datetime()
