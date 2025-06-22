bd1ec3e645e3ad9af980236a9bd5ff1b853bd450
taofeekadisa <adisataofeekabiodun@gmail.com>
2025-06-22 18:59:42 +0200

    update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -103,15 +103,15 @@ class Library:
 
 
 
-classic_book = Book("Pride and Prejudice", "Jane Austen")
-digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+# classic_book = Book("Pride and Prejudice", "Jane Austen")
+# digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+# paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-my_library = Library()
+# my_library = Library()
 
-my_library.add_book(classic_book)
-my_library.add_book(digital_novel)
-my_library.add_book(paper_novel)
+# my_library.add_book(classic_book)
+# my_library.add_book(digital_novel)
+# my_library.add_book(paper_novel)
 
-print(my_library.list_books())
+# print(my_library.list_books())
 
